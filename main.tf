cab3e7b336a9fea11a55b419ba25955f6a02adb4
Jaya <jcpg2048@gmail.com>
2023-03-29 18:41:53 -0300

    final code

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "cc7a6381-c0b9-4005-ab96-5f428e4dd7c0"
-  client_id       = "45bc09d1-98bd-42b5-be12-fa51f009937e"
+  client_id       = "a35d5987-d08d-4d22-b409-6c3a69c08a68"
   client_secret   = "JK98Q~KU2fvzvGjNlrG8b6R_TplmeA2gxBhtXdcA"
   tenant_id       = "c9729b2b-79f3-4668-8a0e-4f5be96118cf"
 }
---
+++ variables.tf
@@ -0,0 +1,21 @@
+# Service Principal Variables
+
+variable "client_id" {
+    description =   "a35d5987-d08d-4d22-b409-6c3a69c08a68"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "JK98Q~KU2fvzvGjNlrG8b6R_TplmeA2gxBhtXdcA"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "cc7a6381-c0b9-4005-ab96-5f428e4dd7c0"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "c9729b2b-79f3-4668-8a0e-4f5be96118cf"
+    type        =   string
+}

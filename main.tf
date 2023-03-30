d3afb2482aa1adf6905b3918f185cc120e35c256
Jaya <jcpg2048@gmail.com>
2023-03-29 23:48:43 -0300

    Commit

--- main.tf
+++ main.tf
@@ -16,137 +16,99 @@ provider "azurerm" {
   tenant_id       = "c9729b2b-79f3-4668-8a0e-4f5be96118cf"
 }
 
- resource "azurerm_resource_group" "test" {
-   name     = "acctestrg"
-   location = "West US 2"
- }
+ resource "azurerm_resource_group" "azurerm" {
+  name     = "Terraform"
+  location = "east US"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraformNetwork"
+  location            = azurerm_resource_group.azurerm.location
+  resource_group_name = azurerm_resource_group.azurerm.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
+resource "azurerm_subnet" "subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.azurerm.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_network_security_group" "NSG" {
+  name                = "TestSecurityGroup"
+  location            = azurerm_resource_group.azurerm.location
+  resource_group_name = azurerm_resource_group.azurerm.name
 
- resource "azurerm_virtual_network" "test" {
-   name                = "acctvn"
-   address_space       = ["10.0.0.0/16"]
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
- }
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
- resource "azurerm_subnet" "test" {
-   name                 = "acctsub"
-   resource_group_name  = azurerm_resource_group.test.name
-   virtual_network_name = azurerm_virtual_network.test.name
-   address_prefixes     = ["10.0.2.0/24"]
- }
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_public_ip" "publicip" {
+  name                = "TestPublicIp"
+  resource_group_name = azurerm_resource_group.azurerm.name
+  location            = azurerm_resource_group.azurerm.location
+  allocation_method   = "Static"
 
- resource "azurerm_public_ip" "test" {
-   name                         = "publicIPForLB"
-   location                     = azurerm_resource_group.test.location
-   resource_group_name          = azurerm_resource_group.test.name
-   allocation_method            = "Static"
- }
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_network_interface" "nic" {
+  name                = "Test-nic"
+  location            = azurerm_resource_group.azurerm.location
+  resource_group_name = azurerm_resource_group.azurerm.name
 
- resource "azurerm_lb" "test" {
-   name                = "loadBalancer"
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_virtual_machine" "main" {
+  name                  = "Terraform-VM"
+  location              = azurerm_resource_group.azurerm.location
+  resource_group_name   = azurerm_resource_group.azurerm.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
 
-   frontend_ip_configuration {
-     name                 = "publicIPAddress"
-     public_ip_address_id = azurerm_public_ip.test.id
-   }
- }
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
 
- resource "azurerm_lb_backend_address_pool" "test" {
-   loadbalancer_id     = azurerm_lb.test.id
-   name                = "BackEndAddressPool"
- }
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
- resource "azurerm_network_interface" "test" {
-   count               = 7
-   name                = "acctni${count.index}"
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
-
-   ip_configuration {
-     name                          = "testConfiguration"
-     subnet_id                     = azurerm_subnet.test.id
-     private_ip_address_allocation = "dynamic"
-   }
- }
-
- resource "azurerm_managed_disk" "test" {
-   count                = 7
-   name                 = "datadisk_existing_${count.index}"
-   location             = azurerm_resource_group.test.location
-   resource_group_name  = azurerm_resource_group.test.name
-   storage_account_type = "Standard_LRS"
-   create_option        = "Empty"
-   disk_size_gb         = "100"
- }
-
- resource "azurerm_availability_set" "avset" {
-   name                         = "avset"
-   location                     = azurerm_resource_group.test.location
-   resource_group_name          = azurerm_resource_group.test.name
-   platform_fault_domain_count  = 3
-   platform_update_domain_count = 3
-   managed                      = true
- }
-
- resource "azurerm_virtual_machine" "test" {
-   count                 = 7
-   name                  = "acctvm${count.index}"
-   location              = azurerm_resource_group.test.location
-   availability_set_id   = azurerm_availability_set.avset.id
-   resource_group_name   = azurerm_resource_group.test.name
-   network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
-   vm_size               = "Standard_DS1_v2"
-
-   # Uncomment this line to delete the OS disk automatically when deleting the VM
-   # delete_os_disk_on_termination = true
-
-   # Uncomment this line to delete the data disks automatically when deleting the VM
-   # delete_data_disks_on_termination = true
-
-   storage_image_reference {
-     publisher = "Canonical"
-     offer     = "UbuntuServer"
-     sku       = "16.04-LTS"
-     version   = "latest"
-   }
-
-   storage_os_disk {
-     name              = "myosdisk${count.index}"
-     caching           = "ReadWrite"
-     create_option     = "FromImage"
-     managed_disk_type = "Standard_LRS"
-   }
-
-   # Optional data disks
-   storage_data_disk {
-     name              = "datadisk_new_${count.index}"
-     managed_disk_type = "Standard_LRS"
-     create_option     = "Empty"
-     lun               = 0
-     disk_size_gb      = "100"
-   }
-
-   storage_data_disk {
-     name            = element(azurerm_managed_disk.test.*.name, count.index)
-     managed_disk_id = element(azurerm_managed_disk.test.*.id, count.index)
-     create_option   = "Attach"
-     lun             = 1
-     disk_size_gb    = element(azurerm_managed_disk.test.*.disk_size_gb, count.index)
-   }
-
-   os_profile {
-     computer_name  = "hostname"
-     admin_username = "testadmin"
-     admin_password = "Password1234!"
-   }
-
-   os_profile_linux_config {
-     disable_password_authentication = false
-   }
-
-   tags = {
-     environment = "staging"
-   }
- }
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version ="latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}

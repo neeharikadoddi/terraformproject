3632714220d74ead7b683281d283556b61e7d993
neeharikadoddi <89979106+neeharikadoddi@users.noreply.github.com>
2023-09-01 22:46:09 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 # Service Principal Variables
 
 variable "client_id" {
-    description =   "89d2feb5-8358-4c67-8693-f3e5c97f4cd0"
+    description =   "eb66c303-11c4-4e7d-9b38-32b809202999"
     type        =   string
 }
 
 variable "client_secret" {
-    description =   ".BF8Q~etJ~dbuHD2JJemkqFbor.T5CQNCHnh1ail"
+    description =   "EkV8Q~uNHLot-lGHW5uHdczCtrwJgEi16Du4Qc-k"
     type        =   string
 }
 
 variable "subscription_id" {
-    description =   "321f31e2-7fb6-48d9-aa03-234a4ecaf9ca"
+    description =   "e509134f-842d-4432-83bf-00c924ab2c07"
     type        =   string
 }
 
 variable "tenant_id" {
-    description =   "3d83ab06-3c39-47e5-8d08-533b0bfdec20"
+    description =   "6231e61e-4168-4f80-a771-4d0f421d5ec9"
     type        =   string
 }
